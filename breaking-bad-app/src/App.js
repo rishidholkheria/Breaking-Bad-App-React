55093f49c93337fb826aa2822b099482b6fc3f9b
Rishi Dholkheria <57285705+rishidholkheria@users.noreply.github.com>
2020-10-18 00:04:53 +0530

    Displaying Fetched Data, Search Done, filter Done

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -3,31 +3,33 @@ import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 const App = () => {
   //items = The data to be fetched
   const [items, setItems] = useState([]);
-
   const [isLoading, setisLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   //useEffect = To make request
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
-      )
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
       console.log(result.data); //data = What we get from this endpoint
       setItems(result.data);
       setisLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
-      <CharacterGrid  isLoading={isLoading} items={items} /> 
+      <Search getQuery={(q) => setQuery(q) } />
+      <CharacterGrid isLoading={isLoading} items={items} />
       {/* passed as props */}
     </div>
   );
--- breaking-bad-app/src/components/characters/CharacterGrid.jsx
+++ breaking-bad-app/src/components/characters/CharacterGrid.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Spinner from "../ui/Spinner";
+import CharacterItem from "./CharacterItem";
 
-const CharacterGrid = ( {items, isLoading } ) => {
-    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
-        {items.map(item => (
-            <h1 key={item.char_id}>{item.name}</h1>
-        ))}
-    </section>)
-}
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <Spinner/>
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+          <CharacterItem key={item.char_id} item={item}></CharacterItem>
+      ))}
+    </section>
+  );
+};
 
-export default CharacterGrid
+export default CharacterGrid;
